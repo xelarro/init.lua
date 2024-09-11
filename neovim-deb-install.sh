797b7096b4a016b36d9c4630191f11f893d9ec88
xelarro <126778814+xelarro@users.noreply.github.com>
2024-09-11 08:55:12 +0200

    Update neovim-deb-install.sh

--- neovim-deb-install.sh
+++ neovim-deb-install.sh
@@ -9,5 +9,5 @@ cd $HOME
 NEOVIM_URL=$(curl -s https://api.github.com/repos/neovim/neovim/releases/latest | jq -r '.assets[] | select(.name == "nvim-linux64.tar.gz") | .browser_download_url')
 
 wget --quiet ${NEOVIM_URL} -O /tmp/nvim-linux64.tar.gz
-tar -xavf /tmp/nvim-linux64.tar.gz $HOME/nvim-linux64
+tar -xavf /tmp/nvim-linux64.tar.gz -C $HOME/
 ln -s $HOME/nvim-linux64/bin/nvim /usr/bin/nvim
