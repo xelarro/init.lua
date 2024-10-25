55f85cbb93e46e03b17464b503aecaff5ac802d9
xelarro <5991gca@gmail.com>
2024-10-25 11:53:42 +0200

    add npm

--- neovim-deb-install.sh
+++ neovim-deb-install.sh
@@ -3,9 +3,9 @@
 set -eou pipefail
 
 if [[ $(id -u 2>/dev/null) -ne 0 ]]; then
-	sudo apt update && sudo apt install -y ripgrep jq curl wget unzip gcc
+	sudo apt update && sudo apt install -y ripgrep jq curl wget unzip gcc npm
 else
-	apt update && apt install -y ripgrep jq curl wget unzip gcc
+	apt update && apt install -y ripgrep jq curl wget unzip gcc npm
 fi
 
 
